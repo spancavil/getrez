612538c1fabd4b67f32e45f34bc34e42ed80b22b
spancavil <soancavil@gmail.com>
2024-05-22 20:45:02 -0300

    Added pino logger and pretty pino. Added validations with class-validator on reservations. Added dockerfile and docker compose, dockerizing app

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,8 +1,21 @@
-import { Prop } from "@nestjs/mongoose";
+import { Prop } from '@nestjs/mongoose';
+import { Type } from 'class-transformer';
+import { IsDate, IsString, IsNotEmpty } from 'class-validator';
 
 export class CreateReservationDto {
-    startDate: Date;
-    endDate: Date;
-    placeId: string;
-    invoiceId: string;
+  //Add class validator as decorators
+  @IsDate()
+  @Type(() => Date) //Will transform the date, ie "12-05-1989" into a valid date
+  startDate: Date;
+  @IsDate()
+  @Type(() => Date)
+  endDate: Date;
+
+  @IsString()
+  @IsNotEmpty()
+  placeId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  invoiceId: string;
 }
