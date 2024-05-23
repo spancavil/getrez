1cc8054198c582464a124570086e739bea5be128
spancavil <soancavil@gmail.com>
2024-05-23 14:46:36 -0300

    Modify .env files, now is local for each app. Added dependencies for authentication. Modify auth module to validate .env file. Added port to .env and brings it at main for both apps. Removed ConfigModule at common database.module, because each app will have it's own .env file.

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -2,11 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { ReservationsModule } from './reservations.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino'
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true})) //With the "whitelist", all the properties that are not validated throught decorators won't be considered.
   app.useLogger(app.get(Logger))
+
+  //With get we will retrieve any injectable. So now instead of hardcoding PORT we call an env variabel
+  const configService = app.get(ConfigService)
+  const port = configService.get('PORT')
   await app.listen(3000);
 }
 bootstrap();
--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { ConfigModule } from '../config/config.module';
 import { ModelDefinition, MongooseModule } from '@nestjs/mongoose';
 
 @Module({
     imports: [MongooseModule.forRootAsync({
         //Use factory to load dependencies from factory pattern.
         //The dependency we will use is an instance of ConfigService that will obtain the specified env variable
-        imports: [ConfigModule],
         useFactory: (configService: ConfigService) => {
             console.log(configService.get('MONGODB_URI'));
             return {
